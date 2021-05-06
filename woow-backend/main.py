fb86f9fae25d756854f4ff063ecf6356f9471a46
bhavyameghnani <meghnani.bhavya@gmail.com>
2021-05-06 22:09:35 +0530

    Basic Backend Structure

--- woow-backend/main.py
+++ woow-backend/main.py
@@ -3,6 +3,10 @@ from flask_cors import CORS,cross_origin
 import json, os, base64
 from datetime import datetime 
 from os import walk
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch(
+    ["https://elastic:RZpr3cb5xWqMKpnzEi2JL22R@884bbc0d58c74e63aae612cee67cf2e7.us-east-1.aws.found.io:9243"]) #This will change to new instance
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
